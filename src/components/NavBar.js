6e5570cc779765865b52dcf523007c2cef359122
taniacuff <tcuff@smu.edu>
2022-10-18 17:45:48 -0500

    Changed navbar.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import Navbar from 'react-bootstrap/Navbar';
 const NavBar = () => {
   return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">Counter</Navbar.Brand>
+      <Navbar.Brand href="#hom">Counter</Navbar.Brand>
     </Navbar>
   );
 }
